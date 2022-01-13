a01e4bf31cb7777a1cea828294a2e33915e72bc2
Laxmi Kanta Dahal <dahalp30@gmail.com>
2022-01-13 15:58:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var numberOfButton=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfButton;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){ 
+        var buttonInnerHTML=this.innerHTML;
+        MakeSound(buttonInnerHTML);
+        
+});
+}
+document.addEventListener("keydown",function(event){
+        MakeSound(event.key);
+});
+
+function MakeSound(key){
+        switch(key){
+                case "w":
+                        var tom1=new Audio("sounds/tom-1.mp3");
+                        tom1.play();
+                        break;
+
+                case "a":
+                        new Audio("sounds/tom-2.mp3").play();
+                        break;
+
+                case "s":
+                        new Audio("sounds/tom-3.mp3").play();
+                        break;
+
+                case "d":
+                        new Audio("sounds/tom-4.mp3").play();
+                        break;
+                case "j":
+                        new Audio("sounds/crash.mp3").play();
+                        break;
+                case "k":
+                        new Audio("sounds/kick-bass.mp3").play();
+                        break;
+                case "l":
+                        new Audio("sounds/snare.mp3").play();
+                        break;
+
+                default:
+                        console.log(buttonInnerHTML);
+                       
+        }
+}
